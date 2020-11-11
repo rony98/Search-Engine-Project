fb71c6851b49afbe8c90a9fca12860f9b47d07c3
Rony Verch <verchrony@gmail.com>
2020-11-11 18:15:02 +0000

    generate ranking starting code

--- app/Http/Controllers/GenerateRanking.php
+++ app/Http/Controllers/GenerateRanking.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class GenerateRanking extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function generateRanking(Request $request)
+    {
+        $query = $request->request->get('param');
+
+        info($query);
+
+        $results = DB::select('select * from info_project');
+        $results = json_decode(json_encode($results), true);
+
+        return $results;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('search'); });
+
+Route::post('/results', 'GenerateRanking@generateRanking')->name('generateRanking');
