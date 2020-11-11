f8bc50b2db966c72c9eb54d2859eabb107775823
Rony Verch <verchrony@gmail.com>
2020-11-11 18:15:02 +0000

    generate ranking starting code

--- app/Http/Controllers/GenerateRanking.php
+++ app/Http/Controllers/GenerateRanking.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class GenerateRanking extends Controller
 {
-    //
+    public function __construct() 
+    {
+
+    }
+
+    public function generateRanking(Request $request)
+    {
+        $query = $request->request->get('param');
+    }
 }
