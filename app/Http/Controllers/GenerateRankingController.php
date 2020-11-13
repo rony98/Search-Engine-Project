1332beddc62a1501c7187113ad8d0c3572c6bcb6
Rony <rony.verch@formtec-international.com>
2020-11-12 22:04:41 -0500

    fix formatting for generate ranking

--- app/Http/Controllers/GenerateRankingController.php
+++ app/Http/Controllers/GenerateRankingController.php
@@ -14,28 +14,28 @@ class GenerateRankingController extends Controller
 
     public function generateRanking($query)
     {
-	// To use the query, just use the $query variable. E.g.
-	// dd($query);
-	// The above command will output query to the page
+	    // To use the query, just use the $query variable. E.g.
+	    // dd($query);
+	    // The above command will output query to the page
 
         $results = DB::select('select * from results');
         $results = json_decode(json_encode($results), true);
 
-	// Results is gonna be a nested array. Each index in first array is an array of description/website. E.g.:
-	// array:1 [▼
-	//     0 => array:2 [▼
-	// 	 "description" => "random description"
-	// 	 "website" => "http://tst.com"
-	//     ]
-	// ]
+	    // Results is gonna be a nested array. Each index in first array is an array of description/website. E.g.:
+	    // array:1 [▼
+	    //     0 => array:2 [▼
+	    // 	     "description" => "random description"
+	    // 	     "website" => "http://tst.com"
+        //     ]
+	    // ]
 
-	// Returns first description
-	// $results[0]["description"];
-	
-	// Returns first website
-	// $results[0]["website"];
+	    // Returns first description
+	    // $results[0]["description"];
 
-	// Leave the return for now. Will update in future with a proper page
+	    // Returns first website
+	    // $results[0]["website"];
+
+	    // Leave the return for now. Will update in future with a proper page
         return redirect()->route('search');
     }
 }
