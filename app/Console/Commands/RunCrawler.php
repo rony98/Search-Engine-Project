977f56f34c881dbc7c9c97ba0c6313d9d0c17039
Rony Verch <verchrony@gmail.com>
2020-11-13 04:59:49 +0000

    crawler done

--- app/Console/Commands/RunCrawler.php
+++ app/Console/Commands/RunCrawler.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use DOMDocument;
+use DB;
 
 class runCrawler extends Command
 {
@@ -34,22 +35,47 @@ class runCrawler extends Command
         parent::__construct();
     }
 
+private function remove_invalid($Str) {  
+  $StrArr = str_split($Str); $NewStr = '';
+  foreach ($StrArr as $Char) {    
+    $CharNo = ord($Char);
+    if ($CharNo == 163) { $NewStr .= $Char; continue; } // keep £ 
+    if ($CharNo > 31 && $CharNo < 127) {
+      $NewStr .= $Char;    
+    }
+  }  
+  return mb_convert_encoding(iconv("UTF-8", "UTF-8//IGNORE", $NewStr), 'UTF-8', 'UTF-8');
+}
+
     private function follow_links($url, $home){
         $doc = new DOMDocument();
-        $doc->loadHTML(file_get_contents($url));
+        @$doc->loadHTML(file_get_contents($url));
 
         $linklist = $doc->getElementsByTagName('a');
 
         foreach ($linklist as $link) {
             $l = $link->getAttribute("href");
-            $full_link = $home.$l;
 
-            if (!in_array($full_link, $this->already_crawled)) {
+	    if (strpos($l, "https://") !== false || strpos($l, "http://") !== false || strpos($l, "www.") !== false) {
+                $full_link = $l;
+            } else {
+                $full_link = $home.$l;
+            }
+
+            if (!in_array($full_link, $this->already_crawled) && substr($full_link, 0, 4) == "http") {
                 $this->already_crawled[] = $full_link;
                 $this->crawling[] = $full_link;
-                echo $full_link.PHP_EOL;
+		echo $full_link.PHP_EOL;
                 // Insert data in the DB
+                
+		$dom = new DOMDocument;
+		@$dom->loadHTMLFile($full_link);
 
+		$html = new \Html2Text\Html2Text($dom->saveHTML($dom->getElementsByTagName('body')->item(0)));
+
+                DB::table('results')->insert([
+                    ['description' => $this->remove_invalid($html->getText()), 'website' => $this->remove_invalid($full_link)]
+                ]);
             }
         }
 
